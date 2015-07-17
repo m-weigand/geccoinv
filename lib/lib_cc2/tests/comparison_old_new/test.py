4d48cc54eac4971312b80f8269d732d6e8db3ead
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-17 09:56:15 +0200

    work on tests

--- lib/lib_cc2/tests/comparison_old_new/test.py
+++ lib/lib_cc2/tests/comparison_old_new/test.py
@@ -6,9 +6,10 @@ import lib_dd.main
 data = np.loadtxt('data.dat')
 frequencies = np.loadtxt('frequencies.dat')
 
+
 settings = {'global_prefix': 'spec_000_',
-            'tausel': 'data_ext',
-            'frequencies': frequencies,
+            'tausel': 'data',
+            'frequencies': frequencies[1:4],
         'Nd': 20,
         'norm_factors': 2.5650970211688082,
         'max_iterations': 20}
@@ -32,6 +33,7 @@ print pars_log.shape
 f_old = old.forward(pars_log)
 f_new = old.forward(pars_log)
 
+print 'forward'
 print f_old.shape
 print f_new.shape
 
@@ -40,7 +42,7 @@ print diff
 
 J_old = old.Jacobian(pars_log)
 J_new = new.Jacobian(pars_log)
-print J_old.shape, J_new.shape
+print 'shapes', J_old.shape, J_new.shape
 diff_J = J_old - J_new
 print 'Jdiff', diff_J
 
@@ -57,19 +59,23 @@ print 'tau_diff', tau_old - tau_new
 
 # numdifftools on old Jacobian
 import numdifftools as nd
+print 'pars_log', pars_log
 
 print 'old'
-Jac_func = lambda x: old.Jacobian(x)
-Jnum_old = nd.Jacobian(Jac_func)
+Jac_func = lambda x: old.forward(x)
+Jnum_old = nd.Jacobian(Jac_func, order=4)
 J_old_num = Jnum_old(pars_log)
 print 'num shape', J_old_num.shape
-diff = J_old - J_old_num
-print 'Numdiff', diff
+diff_old = J_old - J_old_num
+print 'Numdiff', diff_old.min(), diff_old.max()
 
 print('new')
-Jac_func = lambda x: new.Jacobian(x)
-Jnum_new = nd.Jacobian(Jac_func)
+Jac_func = lambda x: new.forward(x)
+Jnum_new = nd.Jacobian(Jac_func, order=4)
 J_new_num = Jnum_new(pars_log)
 print 'num shape', J_new_num.shape
-diff = J_new - J_new_num
-print 'Numdiff', diff
+diff_new = J_new - J_new_num
+print 'Numdiff', diff_new.min(), diff_new.max()
+
+total_diff = np.abs(diff_old - diff_new)
+print 'diff old/new' , total_diff.min(), total_diff.max()
