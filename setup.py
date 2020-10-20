b6c1c5fc794fa1d35b0a5debb4411f20fdd04821
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-10-20 15:03:29 +0200

    bump version to 0.8.7.dev0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 
 # TODO: understand the inclusion of requirements
 version_short = '0.8'
-version_long = '0.8.6'
+version_long = '0.8.7.dev0'
 
 extra = {}
 if sys.version_info >= (3,):
